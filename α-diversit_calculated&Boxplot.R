88a852c06e1b9e566548714251ccd9ed47e0183e
Jian Wang <133335887+Janenoshadow@users.noreply.github.com>
2025-08-28 16:01:16 +0800

    α-diversit_calculated&Boxplot.R

--- α-diversit_calculated&Boxplot.R
+++ α-diversit_calculated&Boxplot.R
@@ -1,5 +1,5 @@
 rm(list = ls())
-#setwd("/your_workpath")
+setwd("/Volumes/xiaowang/zhoushan/新分析/diversity/16S/α-diversity")
 
 #load Rpackage
 library(vegan)
@@ -108,7 +108,8 @@ alpha_boxplot1 <- function(alpha_div, metadata, index = "richness", groupID = "G
   max=max(df[,c(index)])
   min=min(df[,index])
   x = df[,c("group",index)]
-  y = x %>% group_by(group) %>% summarise_(Max=paste('max(',index,')',sep=""))
+  #y = x %>% group_by(group) %>% summarise_(Max=paste('max(',index,')',sep=""))
+  y = x %>% group_by(group) %>% summarise(Max = max(.data[[index]], na.rm = TRUE))
   y=as.data.frame(y)
   rownames(y)=y$group
   df$y=y[as.character(df$group),]$Max + (max-min)*0.05
@@ -154,7 +155,7 @@ alpha_boxplot1 <- function(alpha_div, metadata, index = "richness", groupID = "G
 
 #Input your own data
 #-----------------------------------Alpha_diversity_calculation------------------------------------
-metadata = read.table("sample-metadata_new.txt", header=T, row.names=1, sep="\t", comment.char="", stringsAsFactors = F)
+metadata = read.table("sample-metadata.txt", header=T, row.names=1, sep="\t", comment.char="", stringsAsFactors = F)
 # 预览元数据前3行，注意分组列名
 head(metadata, n = 3)
 # 将Group列转换为因子类型
@@ -163,7 +164,7 @@ metadata$Group <- as.factor(metadata$Group)
 OTUtable <- read.table("ASVtable.txt", header = TRUE, sep = "\t",fill = TRUE)
 
 otu <- OTUtable %>%
-  select(9:58)#discard taxonomy annotation according your own data
+  select(9:58)#discard taxonomy annotation
 otu1 <- t(otu)
 otu <- otu1
 alpha_div <- alpha_diversity(otu)
@@ -173,17 +174,27 @@ shannon_diversity <- alpha_div[,4]
 simpson_diversity <- alpha_div[,5]
 
 str(shannon_diversity)
-sink("shapiro_test_results.txt")
 shannon_diversity <- as.numeric(shannon_diversity)
 simpson_diversity <- as.numeric(simpson_diversity)
+sink("shapiro_test_results.txt")
+print("Shannon diversity Shapiro-Wilk test:")
 shapiro.test(shannon_diversity)
+print("Simpson diversity Shapiro-Wilk test:")
 shapiro.test(simpson_diversity)
 sink()
+
 #------------------------------Visualization-------------------------------------------------------
-#alpha_div = read.table("alpha_diversity.txt", header=T, row.names=1, sep="\t")
-metadata$Group = factor(metadata$Group, levels = c("Nov 2021","Jan 2022","May 2022","Jul 2022","Sep 2022","Nov 2022","Jan 2023","Mar 2023","Jun 2023"))#according your category
+alpha_div = read.table("alpha_diversity.txt", header=T, row.names=1, sep="\t")
+alpha_div$shannon <- as.numeric(alpha_div$shannon)
+#alpha_div$simpson <- as.numeric(alpha_div$simpson)
+#alpha_div$Chao1 <- as.numeric(alpha_div$Chao1)
+metadata1 <- metadata
+metadata$Group <- as.factor(metadata$Group)
+metadata$Group = factor(metadata$Group, levels = c("2111","2201","2205","2207","2209","2211","2301","2303","2306"))
 (p = alpha_boxplot1(alpha_div, metadata, "shannon","Group",facet_name = "shannon"))
-#metadata$Group = factor(metadata$Group, levels = c("Nov 2021","Jan 2022","May 2022","Jul 2022","Sep 2022","Nov 2022","Jan 2023","Mar 2023","Jun 2023"))#according your category
+metadata$Group = factor(metadata$Group, levels = c("2111","2201","2205","2207","2209","2211","2301","2303","2306"))
+(p1 = alpha_boxplot1(alpha_div, metadata, "simpson", "Group",facet_name = "simpson"))
+metadata$Group = factor(metadata$Group, levels = c("2111","2201","2205","2207","2209","2211","2301","2303","2306"))
 (p2 = alpha_boxplot1(alpha_div, metadata, "Chao1", "Group",facet_name = "Chao1"))
 
 ggsave(paste0("Gouqi shannon.svg"), p, width=140, height=90, units="mm")
